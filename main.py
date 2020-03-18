aa8112018a4998ecf006de510f538f71986ddbb6
joris <jorisvddonk@gmail.com>
2020-03-18 14:27:45 +0100

    fix: do not load model every time when generating

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ import gpt_2_simple as gpt2
 from pydantic import BaseModel
 from datetime import datetime
 
+sess = gpt2.start_tf_sess()
+gpt2.load_gpt2(sess, run_name='run1')
+
 
 def generate_text(text: str):
-    # TODO: make this global somehow
-    sess = gpt2.start_tf_sess()
-    gpt2.load_gpt2(sess, run_name='run1')
-    return gpt2.generate(sess,
-                         length=250,
-                         temperature=0.7,
-                         prefix=text,
-                         nsamples=1,
-                         batch_size=1,
-                         run_name='run1',
-                         return_as_list=True
-                         )[0]
+    with sess.graph.as_default():
+        return gpt2.generate(sess,
+                             length=250,
+                             temperature=0.7,
+                             prefix=text,
+                             nsamples=1,
+                             batch_size=1,
+                             run_name='run1',
+                             return_as_list=True
+                             )[0]
 
 
 class Input(BaseModel):
